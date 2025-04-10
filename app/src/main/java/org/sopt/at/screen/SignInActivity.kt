bddd4d4de3ffa739c5d0e52a190b9c0945b2aeb6
nhyeonii <nhyeonii@naver.com>
2025-04-10 19:29:13 +0900

    [del] #1 로그인 실패 시 스낵바 구현

--- app/src/main/java/org/sopt/at/screen/SignInActivity.kt
+++ app/src/main/java/org/sopt/at/screen/SignInActivity.kt
@@ -2,10 +2,8 @@ package org.sopt.at.screen
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -18,18 +16,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import org.sopt.at.MyActivity
 import org.sopt.at.component.CommonTextField
 import org.sopt.at.component.PasswordTextField
@@ -42,18 +45,11 @@ class SignInActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SignInScreen(
-                onSignUpClick = {
-                    startActivity(Intent(this, SignUpActivity::class.java))
-                },
-                onLoginSuccess = {
-                    startActivity(Intent(this, MyActivity::class.java))
-                },
-                loginFailed = {
-                    Toast.makeText(this, "로그인 실패: ID 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            )
+            SignInScreen(onSignUpClick = {
+                startActivity(Intent(this, SignUpActivity::class.java))
+            }, onLoginSuccess = {
+                startActivity(Intent(this, MyActivity::class.java))
+            })
         }
     }
 }
@@ -62,106 +58,92 @@ class SignInActivity : ComponentActivity() {
 fun SignInScreen(
     onSignUpClick: () -> Unit,
     onLoginSuccess: () -> Unit,
-    loginFailed: () -> Unit
 ) {
     var id by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    /*LaunchedEffect(Unit) {
-        registeredId?.let {
-            Toast.makeText(context, "회원가입 성공! 입력한 정보로 로그인하세요.", Toast.LENGTH_SHORT).show()
-        }
-    }*/
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black)
-            .padding(24.dp)
-    ) {
-        Spacer(modifier = Modifier.height(56.dp))
-
-        Text(
-            "TVING ID 로그인",
-            fontSize = 24.sp,
-            color = Color.White,
-            modifier = Modifier.padding(vertical = 20.dp)
-        )
-
-        CommonTextField(
-            value = id,
-            onValueChange = { id = it },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(12.dp))
-
-        PasswordTextField(
-            value = password,
-            onValueChange = { password = it },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(Modifier.height(24.dp))
-
-        Button(
-            onClick = {
-                if (id == registeredId && password == registeredPassword) {
-                    onLoginSuccess()
-                } else {
-                    loginFailed()
-                }
-            },
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(0.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.LightGray,
-                contentColor = Color.Black
-            )
-        ) {
-            Text("로그인하기")
-        }
-
-
-        Spacer(Modifier.height(16.dp))
-
-        Row(
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }, containerColor = Color.Black
+    ) { paddingValues ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 20.dp),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(24.dp)
         ) {
-            Text(
-                text = "아이디 찾기",
-                color = Color.LightGray,
-                fontSize = 16.sp
-            )
-
-            VerticalDivider(
-                modifier = Modifier.height(22.dp),
-                color = Color.DarkGray,
-                thickness = 1.5.dp
-            )
+            Spacer(modifier = Modifier.height(56.dp))
 
             Text(
-                text = "비밀번호 찾기",
-                color = Color.LightGray,
-                fontSize = 16.sp
+                text = "TVING ID 로그인",
+                fontSize = 24.sp,
+                color = Color.White,
+                modifier = Modifier.padding(vertical = 20.dp)
             )
 
-            VerticalDivider(
-                modifier = Modifier.height(22.dp),
-                color = Color.DarkGray,
-                thickness = 1.5.dp
+            CommonTextField(
+                value = id, onValueChange = { id = it }, modifier = Modifier.fillMaxWidth()
             )
 
-            Text(
-                text = "회원가입",
-                color = Color.LightGray,
-                fontSize = 16.sp,
-                modifier = Modifier.clickable { onSignUpClick() }
+            Spacer(Modifier.height(12.dp))
+
+            PasswordTextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier.fillMaxWidth()
             )
+
+            Spacer(Modifier.height(24.dp))
+
+            Button(
+                onClick = {
+                    if (id == registeredId && password == registeredPassword) {
+                        onLoginSuccess()
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("ID 또는 비밀번호가 일치하지 않습니다.")
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.LightGray, contentColor = Color.Black
+                )
+            ) {
+                Text("로그인하기")
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("아이디 찾기", color = Color.LightGray, fontSize = 16.sp)
+
+                VerticalDivider(
+                    modifier = Modifier.height(22.dp), color = Color.DarkGray, thickness = 1.5.dp
+                )
+
+                Text("비밀번호 찾기", color = Color.LightGray, fontSize = 16.sp)
+
+                VerticalDivider(
+                    modifier = Modifier.height(22.dp), color = Color.DarkGray, thickness = 1.5.dp
+                )
+
+                Text(
+                    text = "회원가입",
+                    color = Color.LightGray,
+                    fontSize = 16.sp,
+                    modifier = Modifier.clickable { onSignUpClick() })
+            }
         }
     }
 }
